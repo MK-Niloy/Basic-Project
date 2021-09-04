8e79af2d790d78cfee2552a3a492416aa77472ef
Md Kamrujjaman (Niloy) <84450650+MK-Niloy@users.noreply.github.com>
2021-09-04 11:18:37 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic-Project
+ Web Development Project Two
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php practice</title>
+</head>
+<body>
+   <?php
+   ECHO "My Name is Md. Kamrujjaman <BR>";//Simple Line Output
+    // ($) Variable Define
+   $color ="Black";
+   $COLOR =" White";
+   $colOR =" Green";
+   //php is case-sensitive language
+   echo "My car is " .$color . "<br>";
+   echo "Our campus is " .$COLOR . "<br>"; 
+   echo "Our village is " .$colOR . "<br>";
+   // PHP EXHO AND PRINT STATEMENT 
+   //ECHO STATEMENT
+   $TXT1="LEARN PHP ";
+   $TXT2="W3SCHOOL.COM ";
+   $X="5";
+   $Y="4";
+   ECHO "<H2>" . $TXT1.  "</H2>";
+   ECHO "STUDY PHP AT  " . $TXT2.  "<BR><BR>";
+   ECHO  $X + $Y;
+   //PRINT STATEMENT
+   $TXT3="LEARN PHP ";
+   $TXT4="W3SCHOOL.COM ";
+   $A="5";
+   $B="4";
+   PRINT "<H2>" . $TXT3.  "</H2>";
+   PRINT "STUDY PHP AT  " . $TXT4.  "<BR><BR>";
+   PRINT  $A + $B;
+   ?>
+    
+</body>
+</html>
